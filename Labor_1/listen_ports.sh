9ac7acdb3f9ba37be71521083c47126afdfa47c2
Ernest Gjulioski <gjulioski.ernest@gmail.com>
2025-03-29 16:45:39 +0100

    make it pretty

--- Labor_1/listen_ports.sh
+++ Labor_1/listen_ports.sh
@@ -2,25 +2,26 @@
 
 BLUE='\e[34m'
 RESET='\e[0m'
+RED='\e[31m'
 
 
 # Anleitung:
 usage() {
-    echo "Verwendung: $0 [-c | --command] [-u | --user] [-h | --help]"
+    echo "---------------------------------------------------------------------------"
+    echo -e "Verwendung: $0 ${BLUE}[-c | --command] [-u | --user] [-h | --help]${RESET}"
+    echo "---------------------------------------------------------------------------"
+    echo
     echo "  -c |--command  Zeigt nur den Command aus"
+    echo
     echo "  -u |--user     Zeigt nur die Benutzer-ID mit Port"
+    echo
     echo "  -h |--help     Zeigt diese Hilfemeldung an"
+    echo
+    echo "---------------------------------------------------------------------------"
     exit 0
 }
 
 
-# Prüfen, ob die benötigten Befehle existieren
-for cmd in ss lsof ps awk grep cut sort; do
-    if ! command -v $cmd &> /dev/null; then
-        echo "Fehler: Befehl '$cmd' nicht gefunden."
-        exit 1
-    fi
-done
 
 
 
@@ -37,7 +38,9 @@ case "$1" in
 
     "" ) ;; 
     # Falls kein Argument übergeben wird, wird die Standardoption verwendet
-    *) echo "Fehler: Ungültige Option '$1'"; usage ;;
+    *) echo -e "${RED}Fehler: Ungültige Option '$1'${RESET}";
+    
+     usage ;;
 esac
 
 
@@ -74,6 +77,7 @@ fi
 for port in $ports; do
     pid=$(lsof -iTCP:$port -sTCP:LISTEN -t | head -n 1)
 
+#Print für Command -c und User -u
     if [ -z "$pid" ]; then
         if [ "$option" == "command" ]; then
             printf "%-8s %-50s\n" "$port" "root"
@@ -85,16 +89,13 @@ for port in $ports; do
         continue
     fi
 
-
-
-
+# Holt die User-ID des Prozesses mit der angegebenen PID. 
+# Falls der Prozess nicht existiert, wird die UID auf "0" gesetzt.
     uid=$(ps -o uid= -p "$pid" 2>/dev/null)
     [ -z "$uid" ] && uid="0"
 
 
-
-
-
+#Sobald die UID 0 ist, wird sie auf "root" gesetzt.
     if [ "$option" == "command" ]; then
         cmd=$(ps -o cmd= -p "$pid" 2>/dev/null | awk '{print $1}')
         [ -z "$cmd" ] && cmd="Prozess beendet"
